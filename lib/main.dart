47ce37fa082da5f52f9bc75354c06930754f5d85
dalodev <dalodev9@gmail.com>
2026-03-16 16:15:26 +0700

    add bottom navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:saraba_mobile/ui/common/bottomsheet_navigation/bloc/navigatioin_state.dart';
+import 'package:saraba_mobile/ui/common/bottomsheet_navigation/bloc/navigation_bloc.dart';
+import 'package:saraba_mobile/ui/common/bottomsheet_navigation/bloc/navigation_event.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Widget _buildPage(NavigationTab tab) {
+    switch (tab) {
+      case NavigationTab.dashboard:
+        return Center(child: Text('Dashboard Page'));
+      case NavigationTab.absensi:
+        return Center(child: Text('Absensi Page'));
+      case NavigationTab.pekerjaan:
+        return Center(child: Text('Pekerjaan Page'));
+      case NavigationTab.akun:
+        return Center(child: Text('Akun Page'));
+    }
   }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return BlocProvider(create: (_) => NavigationBloc(), 
+  //   child: Scaffold(
+  //     body: BlocBuilder<NavigationBloc, NavigationState>(
+  //       builder: (context, state) {
+  //         return _buildPage(state.selectedTab);
+  //       },
+  //     ),
+  //     bottomSheet: Container(
+  //       height: 80,
+  //       decoration: BoxDecoration(
+  //         color: Colors.white,
+  //         borderRadius: BorderRadius.only(
+  //           topLeft: Radius.circular(20),
+  //           topRight: Radius.circular(20),
+  //         ),
+  //         boxShadow: [
+  //           BoxShadow(
+  //             color: Colors.black12,
+  //             blurRadius: 10,
+  //             offset: Offset(0, -2),
+  //           ),
+  //         ],
+  //       ),
+  //       child: Row(
+  //         mainAxisAlignment: MainAxisAlignment.spaceAround,
+  //         children: [
+  //           IconButton(
+  //             icon: Icon(Icons.dashboard),
+  //             onPressed: () => context.read<NavigationBloc>().add(NavigateToPage(NavigationTab.dashboard)),
+  //           ),
+  //           IconButton(
+  //             icon: Icon(Icons.check_circle),
+  //             onPressed: () => context.read<NavigationBloc>().add(NavigateToPage(NavigationTab.absensi)),
+  //           ),
+  //           IconButton(
+  //             icon: Icon(Icons.work),
+  //             onPressed: () => context.read<NavigationBloc>().add(NavigateToPage(NavigationTab.pekerjaan)),
+  //           ),
+  //           IconButton(
+  //             icon: Icon(Icons.account_circle),
+  //             onPressed: () => context.read<NavigationBloc>().add(NavigateToPage(NavigationTab.akun)),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //   ));
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return BlocProvider(
+      create: (_) => NavigationBloc(),
+      child: BlocBuilder<NavigationBloc, NavigationState>(
+        builder: (context, state) {
+
+          return Scaffold(
+            body: _buildPage(state.selectedTab),
+
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: state.selectedTab.index,
+
+              onTap: (index) {
+                context.read<NavigationBloc>().add(
+                  NavigateToPage(NavigationTab.values[index]),
+                );
+              },
+
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.dashboard),
+                  label: "Dashboard",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.check_circle),
+                  label: "Absensi",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.work),
+                  label: "Pekerjaan",
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.account_circle),
+                  label: "Akun",
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          );
+        },
       ),
     );
   }
---
+++ lib/ui/absensi/absensi_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AbsensiPage extends StatelessWidget {
+  const AbsensiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Absensi Page'),
+    );
+  }
+} 
